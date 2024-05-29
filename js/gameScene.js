11548bc6a0e2c10541e8ef4793fcb980cdffdfe6
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-29 18:50:10 +0000

    added gameScen.js - yoochan

--- js/gameScene.js
+++ js/gameScene.js
@@ -15,27 +15,39 @@ class GameScene extends Phaser.Scene {
      * this method is the constructor
      */
   
-    createCar () {
-      const carXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number betwen 1 and 1920 
+    createCar (tier) {
+        //car's spawn position 
+      if (tier==1) {
+        let carXPosition = //pass = TIER 1
+        pass
+      } else if(tier==2) {
+        let carXPosition = //pass = TIER 1 + P
+        pass
+      } else {
+        let carXPosition = //pass = TIER 1 + P + R
+        pass
+      }
+      const carXLocation = Math.floor(carXPosition * 1920) + 1 // this will get a number betwen 1 and 1920 
       let carXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number betwee 1 and 50
-      alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
+      carXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
       const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
       anAlien.body.velocity.y = 200
       anAlien.body.velocity.x = alienXVelocity
-      this.alienGroup.add(anAlien)
+      this.carGroup.add(aCar)
     }
   
     constructor() {
       super({ key: "gameScene"})
   
       this.background = null
-      this.ship = null
+      this.car = null
       this.fireMissile = false
-      this.score = 0
-      this.scoreText = null
-      this.scoreTextStyle = {font: '65px Arial', fill:'#ffffff', align: 'center'}
-      this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', align: 'center' }
-  }
+      this.money = 0
+      this.moneyText = null
+      this.level = 0
+      this.levelText = null
+      this.infoTextStyle = {font: '65px Arial', fill:'#ffffff', align: 'center'}
+    }
   
     /**
      * Can be defined on your own Scenes.
